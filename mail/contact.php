981a5776bd66a155760711d652d8b5a12b3a5733
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-19 17:48:32 -0300

    ok

--- mail/contact.php
+++ mail/contact.php
@@ -67,16 +67,10 @@ $mail->Subject = 'Contact Request from bootstrapfriendly demo form';
 $mail->isHTML(true);
 ?>
 
-<!doctype html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
 <?php
 $mail->Body = "
 
@@ -110,7 +104,7 @@ $mail->Body = "
 if (!$mail->send()) {
 //The reason for failing to send will be in $mail->ErrorInfo
 //but you shouldn't display errors to users - process the error, log it on your server.
-
+http_response_code(500);
 echo "<div class='alert alert-danger'>Sorry, something went wrong. Please try again later.</div>";
 } else {
 
